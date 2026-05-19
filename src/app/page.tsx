84dcdc1f500058f748590eb20eea051aa698c657
Azzam Azhari <azzamazhari.dev@gmail.com>
2026-05-19 23:19:34 +0700

    Authentication System

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { DarkmodeToggle } from '@/components/common/darkmode-toggle';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <Input />
-      <Button>Hello</Button>
-      <DarkmodeToggle />
+    <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
+      <h1 className="text-4xl font-semibold">Azzam</h1>
+      <Link href="/admin">
+        <Button className="bg-yellow-500 text-white">Access Dashboard</Button>
+      </Link>
     </div>
   );
 }
