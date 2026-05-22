0f321dfc5bd87a8247538c759a866d91d042e686
Azzam Azhari <azzamazhari.dev@gmail.com>
2026-05-23 01:10:15 +0700

    WPU: Management Order

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
   SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
-
-  // Hapus NEXT_PUBLIC_ di sini agar cocok dengan yang ada di file .env
   SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY || '',
+
+  MIDTRANS_API_URL: process.env.NEXT_PUBLIC_MIDTRANS_API_URL || '',
+  MIDTRANS_CLIENT_KEY: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY || '',
+  MIDTRANS_SERVER_KEY: process.env.MIDTRANS_SERVER_KEY || '',
 };
